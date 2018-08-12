9d6ea4ae7325b7f9bbaf6efe0628bab255542eab
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-12 16:37:10 -0400

    Converting to Spark types
    Converting to Spark types

--- Certification-Notes/SparkCode/src/org/apache/spark/chapter6/DataTypesExample.scala
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter6/DataTypesExample.scala
@@ -22,6 +22,23 @@ object DataTypesExample {
     // Change the default shuffle partition from 200 to 5
     spark.conf.set("spark.sql.shuffle.partition", "5")    
     
+    // DataFrameStatFunctions - Hold variety of statistically related function 
+    // DataFrameNaFunctions - refers function that are relevant when working with null data.
+    
+    // Reading the data
+    val df = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("D:\\Github\\ApacheSpark\\Certification-Notes\\data\\retail-data\\by-day\\2010-12-01.csv")
 
+    // Print the Schema, Create Temporary table and show all the data
+    df.printSchema()
+    df.createOrReplaceTempView("dfTable")
+    df.show()
+    
+    //*** - Converting to Spark type - using lit
+    import org.apache.spark.sql.functions.lit
+    df.select(lit(5), lit("five"), lit(5.0)).show()
+    
   }
 }
