9b4918c4d14b7567e833e8b7225b204a827cbb17
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-12 15:57:52 -0400

    DataTypes Example
    DataTypes Example

---
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter6/DataTypesExample.scala
@@ -0,0 +1,27 @@
+package org.apache.spark.chapter6
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.log4j._
+
+object DataTypesExample {
+  
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR) 
+    
+    // Create Spark Session
+    val spark = SparkSession
+      .builder()
+      .appName("SchemaExample")
+      .master("local[*]")
+      .getOrCreate()
+      
+    // Change the default shuffle partition from 200 to 5
+    spark.conf.set("spark.sql.shuffle.partition", "5")    
+    
+
+  }
+}
