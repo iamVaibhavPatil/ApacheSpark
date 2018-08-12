97d30e90da0b067ed9ce4d88b84b6389736da0d3
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-12 17:11:25 -0400

    Working with Boolean
    Working with Boolean

--- Certification-Notes/SparkCode/src/org/apache/spark/chapter6/DataTypesExample.scala
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter6/DataTypesExample.scala
@@ -40,5 +40,59 @@ object DataTypesExample {
     import org.apache.spark.sql.functions.lit
     df.select(lit(5), lit("five"), lit(5.0)).show()
     
+    //*** - Working with Boolean
+    import org.apache.spark.sql.functions.col
+    df.where(col("InvoiceNo").equalTo(536365))
+      .select("InvoiceNo", "Description")
+      .show(5, false)
+      
+    // Use -> === or =!=
+    df.where(col("InvoiceNo") === 536365)
+      .select("InvoiceNo", "Description")
+      .show(5, false)
+    
+   // Use predicate as an expression
+   df.where("InvoiceNo = 536365").show(5, false)
+   
+   // Use of does not equal to(<>)
+   df.where("InvoiceNo <> 536365").show(5, false)
+   
+   // and - We can either specify and statement as a single statement or chained them to have clear code. Spark flatten them and executes them at once
+   // or - statement needs to be specified in single statement
+   val priceFilter = col("UnitPrice") > 600
+   val descriptionFilter = col("Description").contains("POSTAGE")
+   df.where(col("StockCode").isin("DOT"))
+     .where(priceFilter.or(descriptionFilter))
+     .show()
+     
+   // Add Boolean Column to DataFrame using Filters
+   val DOTCodeFilter = col("StockCode") === "DOT"
+   df.withColumn("isExpensive", DOTCodeFilter.and(priceFilter.or(descriptionFilter)))
+     .where("isExpensive")
+     .select("unitPrice", "isExpensive")
+     .show(5)
+     
+   // eqNullSafe -> if we have null data, we can use eqNullSafe instead of equalTo
+   df.where(col("Description").eqNullSafe("hello")).show()
+   
+   // Using DataFrame API vs Spark SQL - Both will give same performance
+   import org.apache.spark.sql.functions.{expr, col, not}
+   
+   // With DataFrame
+   df.withColumn("isExpensive", not(col("UnitPrice").leq(250)))
+     .filter("isExpensive")
+     .select("Description", "UnitPrice")
+     .show(5)
+     
+   // With Spark SQL
+   df.withColumn("isExpensive", expr("NOT UnitPrice <= 250"))
+     .filter("isExpensive")
+     .select("Description", "UnitPrice")
+     .show(5)   
+   
+     
+     
+   //*** - Working with Numbers
+    
   }
 }
