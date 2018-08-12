9f4cbd4fa66005030049084d71633d469c04a51b
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-12 11:25:02 -0400

    types tp spark
    types tp spark

--- Certification-Notes/SparkCode/src/org/apache/spark/chapter5/SchemaExample.scala
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter5/SchemaExample.scala
@@ -27,25 +27,27 @@ object SchemaExample {
     
     val myManualSchema = StructType(Array(
         StructField("DEST_COUNTRY_NAME", StringType, true),
-        StructField("ORINGIN_COUNTRY_NAME", StringType, true),
+        StructField("ORIGIN_COUNTRY_NAME", StringType, true),
         StructField("count", LongType, false, Metadata.fromJson("{\"hello\":\"world\"}"))
         ))
     
     val df = spark.read.format("json").schema(myManualSchema).load("D:\\Github\\ApacheSpark\\Certification-Notes\\data\\flight-data\\json\\2015-summary.json") 
     
+    // Select and SelectExpr
     df.printSchema()  
     
     df.select("DEST_COUNTRY_NAME").show(2)
     
-    df.select("DEST_COUNTRY_NAME","ORINGIN_COUNTRY_NAME").show(2)
-    
+    df.select("DEST_COUNTRY_NAME","ORIGIN_COUNTRY_NAME").show(2)
+
     df.selectExpr(
     "*", // include all original columns
-    "(DEST_COUNTRY_NAME = ORINGIN_COUNTRY_NAME) as withinCountry").show(2)
+    "(DEST_COUNTRY_NAME = ORIGIN_COUNTRY_NAME) as withinCountry").show(2)
   
     df.selectExpr("avg(count)", "count(distinct(DEST_COUNTRY_NAME))").show(2)
-    
-    
-    
+   
+    // Converting to Spark Types(literals)
+    import org.apache.spark.sql.functions.{lit, expr}
+    df.select(expr("*"), lit(1).as("One")).show(2)
   }
 }
