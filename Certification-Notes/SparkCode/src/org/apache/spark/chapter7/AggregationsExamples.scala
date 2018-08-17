a82f08a0d0bf75c37db798b9a474a2a929882028
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-17 18:12:20 -0400

    Aggregation examples
    Aggregation examples

---
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter7/AggregationsExamples.scala
@@ -0,0 +1,28 @@
+package org.apache.spark.chapter7
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import org.apache.log4j._
+
+object AggregationsExamples {
+
+  def main(args: Array[String]) {
+    
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+    // Create Spark Session
+    val spark = SparkSession
+      .builder()
+      .appName("AggregationsExamples")
+      .master("local[*]")
+      .getOrCreate()
+    
+    // Change the default shuffle partition from 200 to 5
+    spark.conf.set("spark.sql.shuffle.partition", "5")      
+      
+    
+      
+  }
+}
