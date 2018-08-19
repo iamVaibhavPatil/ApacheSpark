db446472ce0db6c53ecd9f83a37c3481cd3d59de
vaibhavpatilai <6760119+vaibhavpatilai@users.noreply.github.com>
2018-08-19 07:35:51 -0400

    Joins
    Joins

--- Certification-Notes/SparkCode/src/org/apache/spark/chapter8/JoinsExamples.scala
+++ Certification-Notes/SparkCode/src/org/apache/spark/chapter8/JoinsExamples.scala
@@ -24,8 +24,50 @@ object JoinsExamples {
     
     
     /* **** Join Expression  *****
+     * A join brings together two sets of data, the left and the right, by comparing the value of one or more keys of the left and
+     * right and evaluating the result of join expression that determines whether Spark should bring together the left set of data
+     * with right set of data.
      * 
+     * equi-join - Compares whether specified keys in your left and right datasets are equal. If equal, Spark will combine the left and right datasets.
+     * We can even use complex types and check whether keys exists within array when you perform join.
      * */
+    
+    
+    /* **** Join Types  *****
+     * Join type determines what should be in the result set.
+     * 
+     * 1) Inner joins - Keep rows with keys that exist in the left and right datasets
+     * 2) Outer joins - Keep rows with keys that exist in either left or right datasets
+     * 3) Left Outer joins - Keep rows with keys that exist in left dataset
+     * 4) Right Outer joins -  Keep rows with keys that exist in right dataset
+     * 5) Left Semi joins - keep the rows in left dataset where keys appear in the right dataset
+     * 6) Left Anti joins - Keep the rows in left dataset where keys do not appear in the right dataset
+     * 7) Natural joins - perform a join by implicitly matching the columns between the two datasets with the same names
+     * 8) Cross or Cartesian joins - match every row in the left dataset with every row in right dataset
+     * */
+    import spark.implicits._   //Without this import toDF will not work in Seq
+    
+    val person = Seq(
+      (0, "Bill Chambers", 0, Seq(100)),
+      (1, "Matei Zaharia", 1, Seq(500, 250, 100)),
+      (2, "Michael Armbrust", 1, Seq(250, 100)))
+    .toDF("id", "name", "graduat_program", "spark_status")
+    
+    val graduatProgram = Seq(
+      (0, "Masters", "School of Information", "UC Berkeley"),
+      (1, "Masters", "EECS", "UC Berkeley"),
+      (2, "Ph.D.", "EECS", "UC Berkeley"))
+    .toDF("id", "degreee", "department", "school")
+    
+    val sparkStatus = Seq(
+      (500, "Vice President"),
+      (250, "PMC Member"),
+      (100, "Contributor"))
+    .toDF("id", "status")
+    
+    
+    
+    
     
     
   }
